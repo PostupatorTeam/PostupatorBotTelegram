f1001862665eb7e306f3806e2a236f5cccf6118c
DenisKrivoruchko11 <den89181827071@mail.ru>
2021-01-04 17:06:35 +0000

    add spbu parser

--- config/config.py
+++ config/config.py
@@ -4,3 +4,6 @@ ranepa_base_link = "https://lk.ranepa.ru/pk/list.php?FT=1&FL=0"
 
 etu_base_link = "https://etu.ru/"
 etu_link_to_list = "https://etu.ru/ru/abiturientam/priyom-na-1-y-kurs/podavshie-zayavlenie/"
+
+spbu_base_link = "https://cabinet.spbu.ru/Lists/1k_EntryLists/"
+spbu_link_to_list = "https://cabinet.spbu.ru/Lists/1k_EntryLists/index_comp_groups.html"
